a8f6008d4c324f564dadc59cb9ff7e9e705023ef
zakmira <zakmira2007@gmail.com>
2026-04-24 16:29:37 +0300

    Буквы

---
+++ ConsoleAppLetters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class ArrayExample
+{
+    /// <summary>
+    /// Точка входа: итерация символов в массиве, формирование имени и сообщения.
+    /// </summary>
+    static void Main()
+    {
+        char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
+        string name = "";
+        int[] a = new int[10];
+        for (int i = 0; i < letters.Length; i++)
+        {
+            name += letters[i];
+            a[i] = i + 1;
+            SendMessage(name, a[i]);
+        }
+        Console.ReadKey();
+    }
+
+    /// <summary>
+    /// Приветствие с именем и числом.
+    /// </summary>
+    /// <param name="name">Получившееся имя.</param>
+    /// <param name="msg">Число для вывода в сообщении.</param>
+    static void SendMessage(string name, int msg)
+    {
+        Console.WriteLine("Hello, " + name + "! Count to " + msg);
+    }
+}
